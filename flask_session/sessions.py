0a6f0542a8e48ed6663d9dd1e93b68a050e7d815
Andy Altepeter <andyaltepeter@invenshure.com>
2015-10-15 18:11:07 -0500

    make sqlalchemy session work with python3

--- flask_session/sessions.py
+++ flask_session/sessions.py
@@ -483,7 +483,7 @@ class SqlAlchemySessionInterface(SessionInterface):
         if saved_session:
             try:
                 val = saved_session.data
-                data = self.serializer.loads(str(val))
+                data = self.serializer.loads(val)
                 return self.session_class(data, sid=sid)
             except:
                 return self.session_class(sid=sid)
